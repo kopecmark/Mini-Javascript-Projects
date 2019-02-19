5e4257c12bb51ca0da0f7f58fb39bd6cd0afe218
Mark Kopec <mark.kopec@gmail.com>
2019-02-18 20:32:38 -0700

    Update button when video paused or playing

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -12,6 +12,15 @@ function togglePlay() {
   video[method]();
 }
 
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+  console.log(icon);
+}
+
 
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
 toggle.addEventListener('click', togglePlay);
