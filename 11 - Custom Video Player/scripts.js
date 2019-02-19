999e2ab43e6ce714f1ccc2b3ec93abcf168552c3
Mark Kopec <mark.kopec@gmail.com>
2019-02-18 20:40:48 -0700

    Create skip function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -18,9 +18,14 @@ function updateButton() {
   console.log(icon);
 }
 
+function skip(){
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
